fe383c26a2472efc11fdcf2733e3bf13a7a8e033
caleb <shawcaleb1688@gmail.com>
2023-06-29 20:49:16 -0400

    added axios and retrieved data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect }from "react";
 import "./App.css";
-
+import axios from "axios";
 function App() {
+
+useEffect(() => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY') 
+  .then (res => {
+    console.log(res);
+  })
+  .catch(err => console.error(err))
+},[])
+
+
+
   return (
     <div className="App">
       <p>
